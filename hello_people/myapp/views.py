6557fffd1314199a02cdda55833915d605266331
Noothankumarkpmg <pgnoothanprasad2000@gmail.com>
2020-12-18 16:21:35 +0530

    commiting a text file

---
+++ hello_people/myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+
+  return HttpResponse("hello India - welcome -!! Its jangp webpage")
